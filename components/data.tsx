ebc40844cbef7a22a317f0a97808042c9282f4bb
Gabriel Abubakar <gabrielogrima@gmail.com>
2022-08-29 00:53:47 +0100

    home page and data setup

---
+++ components/data.tsx
@@ -0,0 +1,18 @@
+// define the type for each currency item
+export type Currency = {
+    id: string,
+    name: string,
+    symbol: string
+}
+
+// create the array of choosable currencies and their query values ()
+export const currencyData: Currency[] = [
+    { id: "bitcoin", name: "Bitcoin", symbol: "BTC" },
+    { id: "ethereum", name: "Ethereum", symbol: "ETC" },
+    { id: "dogecoin", name: "Dogecoin", symbol: "DOGE" },
+    { id: "BNB", name: "Binancecoin", symbol: "BNB" },
+    { id: "tether", name: "Tether", symbol: "USDT" },
+    { id: "ripple", name: "XRP", symbol: "XRP" },
+    { id: "solana", name: "Solana", symbol: "SOL" },
+    { id: "shiba-inu", name: "Shiba Inu", symbol: "SHIB" },
+]
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,89 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
+import { useState, useEffect } from 'react'
+import { currencyData, Currency } from '../components/data'
 
 
 const Home: NextPage = () => {
+
+    const [price, setPrice] = useState('')
+    const [date, setDate] = useState('')
+    const [currency, setCurrency] = useState('')
+
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault()
+        setDate('')
+        setPrice('')
+    }
+
+
+    const fetchData = async () => {
+        await fetch('')
+            .then((res) => {
+                res.json()
+            })
+            .then(() => {
+
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+
+    useEffect(() => {
+        console.log()
+    }, [date])
+
+
     return (
         <div>
+            <h1>Crypto Time Travel🕑</h1>
+            <p>
+                Ever wondered how much you would have made if you&apos;d bought Bitcoin 6 years ago when that your weird pal told you to? Well, here you are, just input the amount in dollars, choose the coin and the date in the past and find out how much you could have gotten (or lost) over the years.
+            </p>
+
+            <h2>What if I had HODL</h2>
+            <form>
+                <label htmlFor="price">$ USD</label> <br />
+                <input
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                    type="number"
+                    name='price' /> <br /> <br />
+
+
+                <label htmlFor="select-currency">IN:</label> <br />
+                <select name="select-currency" id="select">
+                    {
+                        currencyData && currencyData.map((item: Currency, i: number) => (
+                            <option value={item.id} key={i}>{item.name} ({item.symbol})</option>
+                        ))
+                    }
+                </select> <br /> <br />
+
+                <label htmlFor="date">ON:</label> <br />
+                <input type="date" name='date' value={date} onChange={(e) => setDate(e.target.value)} /> <br /> <br />
+
+
+                <button onClick={handleSubmit}>
+                    <Link href={{
+                        pathname: "/results",
+                        query: {
+                            price,
+                            date
+                        }
+                    }}>
+                        <a>
+                            Find out what it would be worth today
+                        </a>
+                    </Link>
+                </button>
+
+
+            </form>
+
+
 
         </div>
     )
