881b25097b16b71980cb7176d97c102c4cd25c64
Gabriel Abubakar <gabrielogrima@gmail.com>
2022-09-09 20:56:27 +0100

    created tests for result page

---
+++ __tests__/ResultPage.test.js
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Results from '../pages/results'
+
+const mockFn = jest.fn()
+// const na = {
+//     name: "gabe",
+//     price: 123,
+//     date: "24/06/2000"
+// }
+
+
+describe("Tests for the results page", () => {
+    it("Should render out results after fetch", () => {
+        render(
+            // <Results />
+        )
+    })
+})
