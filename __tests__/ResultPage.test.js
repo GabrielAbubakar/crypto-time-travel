e08a5a24991907df83765b7f1e6bed4be513a6af
Gabriel Abubakar <gabrielogrima@gmail.com>
2022-09-10 18:46:12 +0100

    mock useRouter hook

--- __tests__/ResultPage.test.js
+++ __tests__/ResultPage.test.js
@@ -1,19 +1,36 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Results from '../pages/results'
+import { RouterContext } from 'next/dist/shared/lib/router-context'
+import { createMockRouter } from '../test-utils/createMockRouter'
 
-const mockFn = jest.fn()
-// const na = {
-//     name: "gabe",
-//     price: 123,
-//     date: "24/06/2000"
-// }
+
+// mocking out the router and giving it query
+// jest.mock("next/router", () => ({
+//     useRouter: () => ({
+//         query: {}
+//     })
+// }))
 
 
 describe("Tests for the results page", () => {
+    // test 1
     it("Should render out results after fetch", () => {
         render(
-            // <Results />
+            <RouterContext.Provider
+                value={createMockRouter({
+                    query: {
+                        price: '100',
+                        coin: 'bitcoin',
+                        date: '09/10/2022'
+                    }
+                })}>
+                <Results />
+            </RouterContext.Provider>
         )
+
+
+        // const elem = screen.getByText(/bitcoin/i)
+        // expect(elem).toBeInTheDocument()
     })
 })
